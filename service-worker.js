2683d7c811c03d6f926a898c4a338774b9155151
Olaitan Toluwanimi Michael <101925362+Theomnitron@users.noreply.github.com>
2025-08-14 12:03:22 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+// service-worker.js
+// A minimal service worker. Since you don't need offline functionality,
+// this can be very basic. It just needs to exist.
+self.addEventListener('fetch', function(event) {
+  // You can add caching logic here later if needed,
+  // but for now, we'll just let fetches go through.
+});
